e51dab25f89da5faaf4785dad41b80d339000c38
BeCoded126 <77804389+BeCoded126@users.noreply.github.com>
2024-12-07 00:38:26 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const who = ["MY DOG", "MY CAT", "MY GRANDPA", "MY GOAT"];
+const action = ["GULPED", "STEPPED ON", "DESTROYED", "HIT"];
+
+const item = ["MY PIZZA", "MY AUNTIE", "MY NIGHTSTAND", "MY CAR"];
+
+const when = [
+  "LAST NIGHT",
+  "WHILE I WAS NAPPING",
+  "WHEN I WAS AT WORK",
+  "WHEN I WAS IN THE BATHROOM"
+];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const randomExcuse = Math.floor(Math.random() * who.length);
+  const randomExcuse2 = Math.floor(Math.random() * action.length);
+  const randomExcuse3 = Math.floor(Math.random() * item.length);
+  const randomExcuse4 = Math.floor(Math.random() * when.length);
+  document.getElementById("excuse").innerHTML =
+    `${who[randomExcuse]}` +
+    ` ${action[randomExcuse2]}` +
+    ` ${item[randomExcuse3]}` +
+    ` ${when[randomExcuse4]}` +
+    `.`;
 };
